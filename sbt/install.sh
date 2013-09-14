c2515ad1c9ec60d965cd5b4ea413a457a5487b57
khilnani <nik@khilnani.org>
2013-09-14 14:33:31 -0400

    Added sbt

---
+++ sbt/install.sh
@@ -0,0 +1,13 @@
+#!/bin/sh
+
+# sudo port install sbt
+
+VER=0.13.0
+
+curl -O http://repo.typesafe.com/typesafe/ivy-releases/org.scala-sbt/sbt-launch/$VER/sbt-launch.jar 
+sudo mv -f sbt-launch.jar /opt/local/bin/
+sudo cp -f sbt /opt/local/bin
+sudo chmod u+x /opt/local/bin/sbt
+sbt
+
+
