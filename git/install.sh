5eb514e8224a70daca278ce21d8d4d0a138f1a38
Nik <nik@khilnani.org>
2013-12-11 15:55:16 -0500

    Update install.sh

--- git/install.sh
+++ git/install.sh
@@ -1,3 +1,7 @@
 #!/bin/sh
 
 cp -f ./.gitconfig ~/
+git config --global user.name "khilnani"
+git config --global user.email "nik@khilnani.org"
+git config --global color.ui true
+git config --global --edit
