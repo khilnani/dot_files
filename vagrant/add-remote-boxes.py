35d8f707fd6f698193424d7c3fb8242389e2afac
Nik <nik@khilnani.org>
2013-12-14 16:10:16 -0500

    Check if a box exists before adding

--- vagrant/add-remote-boxes.py
+++ vagrant/add-remote-boxes.py
@@ -6,9 +6,9 @@ import subprocess
 
 op = ''
 boxes = [
-	  {'name': 'centos-6.5',  'url': 'https://dl.dropboxusercontent.com/u/26568959/VagrantBoxes/CentOS-6.5-i386.box'},
-	  {'name': 'ubuntu-12',   'url': 'https://dl.dropboxusercontent.com/u/26568959/VagrantBoxes/CentOS-6.5-i386.box'}
-	]
+          {'name': 'centos-6.5',  'url': 'https://dl.dropboxusercontent.com/u/26568959/VagrantBoxes/CentOS-6.5-i386.box'},
+          {'name': 'ubuntu-12',   'url': 'https://dl.dropboxusercontent.com/u/26568959/VagrantBoxes/CentOS-6.5-i386.box'}
+        ]
 
 try:
   op = subprocess.check_output('vagrant box list', shell=True)
@@ -19,9 +19,8 @@ except:
   print 'Unable to execute: vagrant box list'
 else:
   for box in boxes:
-    if op.find(box['name']) == -1:
+    if op.strip().find(box['name'] + '(') == -1:
       print('Adding Box {} from {}'.format(box['name'],box['url']))
       os.system('vagrant box add {} {}'.format(box['name'], box['url']))
     else:
       print('Box {} already exists.'.format(box['name']))
-
