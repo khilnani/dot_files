ad2056eb3e9f633b32e37cac052f7406caa56702
Nik <nik@khilnani.org>
2013-12-14 08:21:54 -0500

    Update add-remote-boxes.sh

--- vagrant/add-remote-boxes.sh
+++ vagrant/add-remote-boxes.sh
@@ -1,5 +1,5 @@
 #!/bin/sh
 
-vagrant box add centos-6.5 CentOS-6.5-i386.box
-vagrant box add ubuntu-12 Ubuntu-12.04.3-i386.box
+vagrant box add centos-6.5 https://dl.dropboxusercontent.com/u/26568959/VagrantBoxes/CentOS-6.5-i386.box
+vagrant box add ubuntu-12 https://dl.dropboxusercontent.com/u/26568959/VagrantBoxes/CentOS-6.5-i386.box
 
