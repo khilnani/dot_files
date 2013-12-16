64d0a0462e650132a2d98ebe1e2475506e4e1f44
khilnani <nik@khilnani.org>
2013-12-15 22:47:32 -0500

    checked in vagrant keys for convinience

--- vagrant/download-vagrant-ssh-keys.sh
+++ vagrant/download-vagrant-ssh-keys.sh
@@ -1,6 +1,6 @@
 #!/bin/sh -x
 
-url -O https://raw.github.com/mitchellh/vagrant/master/keys/vagrant
+curl -O https://raw.github.com/mitchellh/vagrant/master/keys/vagrant
 curl -O https://raw.github.com/mitchellh/vagrant/master/keys/vagrant.pub
 
 chmod 600 vagrant.pub
