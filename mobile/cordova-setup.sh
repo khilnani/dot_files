5ace1a57719b38511404db4758f32f42ac1ae6f6
Nik <nik@khilnani.org>
2014-01-31 23:22:43 -0500

    Update cordova-setup.sh

--- mobile/cordova-setup.sh
+++ mobile/cordova-setup.sh
@@ -6,3 +6,4 @@ cordova platforms add android
 cordova plugin add org.apache.cordova.device
 cordova plugin add org.apache.cordova.console
 cordova plugin add org.apache.cordova.dialogs
+cordova plugin add org.apache.cordova.geolocation
