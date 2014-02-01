40d84480c476e9896a7487ffe8c9270ef9060e2e
Nik <nik@khilnani.org>
2014-01-31 23:27:00 -0500

    Update cordova-setup.sh

--- mobile/cordova-setup.sh
+++ mobile/cordova-setup.sh
@@ -8,3 +8,4 @@ cordova plugin add org.apache.cordova.console
 cordova plugin add org.apache.cordova.dialogs
 cordova plugin add org.apache.cordova.geolocation
 cordova plugin add org.apache.cordova.contacts
+cordova plugin add org.apache.cordova.camera
