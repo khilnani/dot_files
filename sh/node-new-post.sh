8152aab64c0f1906059b1189a626a6b2094daccb
Nik <nik@khilnani.org>
2014-02-02 21:02:21 -0500

    Update node-new-post.sh

--- sh/node-new-post.sh
+++ sh/node-new-post.sh
@@ -1,11 +1,12 @@
 #!/usr/bin/env sh
 
-npm install gulp gulp-util --save-dev 
-npm install gulp-jade --save-dev
-npm install browserify --save-dev
 npm install coffee --save-dev
 
+npm install browserify --save-dev
 
+npm install gulp gulp-util --save-dev 
+npm install gulp-jade --save-dev
+npm install gulp-coffee --save-dev
 
 npm install express --save
 npm install jade --save
