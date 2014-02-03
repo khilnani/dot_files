809e929fdb5940af084d746d88094f2448a11938
Nik <nik@khilnani.org>
2014-02-02 20:44:30 -0500

    Update node-new-post.sh

--- sh/node-new-post.sh
+++ sh/node-new-post.sh
@@ -4,5 +4,8 @@ npm install gulp --save-dev
 npm install browserify --save-dev
 npm install coffee --save-dev
 
+npm install express --save
+npm install jade --save
+npm install less-middleware --save
 npm install async --save
 
