fc480b4daf1c7edb75294f42e886ea87068b4d13
khilnani <nik@khilnani.org>
2016-11-28 14:04:34 -0500

    added

---
+++ sh/dtag.sh
@@ -0,0 +1,16 @@
+#!/bin/sh
+
+function usage() {
+    echo "$0 IMAGE TAG"
+}
+
+function main() {
+    docker tag $1:latest $1:$2
+    docker push $1:$2
+}
+
+if [ "$#" -ne 2 ]; then
+    usage
+else
+   main $1 $2
+fi
