ecc3350dc8497667f89a0d531187b4fdccc1699e
Nik <nik@khilnani.org>
2017-03-02 22:20:59 -0500

    Update _ohmyzsh.sh

--- zsh/_ohmyzsh.sh
+++ zsh/_ohmyzsh.sh
@@ -1,6 +1,7 @@
 #!/bin/sh
 
-sh -c "$(curl -fsSL https://raw.githubusercontent.com/robbyrussell/oh-my-zsh/master/tools/install.sh)"
+#sh -c "$(curl -fsSL https://raw.githubusercontent.com/robbyrussell/oh-my-zsh/master/tools/install.sh)"
+curl -L http://install.ohmyz.sh | sh
 
 git clone https://github.com/powerline/fonts.git
 
