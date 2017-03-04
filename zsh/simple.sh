d77fe54f6e0a249c5c7c6badd07f56485d4041c6
khilnani <nik@khilnani.org>
2017-03-04 23:49:26 +0000

    added vimrc

--- zsh/simple.sh
+++ zsh/simple.sh
@@ -2,3 +2,4 @@
 apt-get install -y zsh git-core vim
 curl -L http://install.ohmyz.sh | sh
 sed -ie 's/robbyrussell/avit/g' ~/.zshrc
+curl https://raw.githubusercontent.com/khilnani/dot_files/master/vi/vimrc_safe -o ~/.vimrc
