d481ea1f5c2c49734a694acef086a94f97b30925
Nik <nik@khilnani.org>
2017-03-18 20:26:20 -0400

    Update simple.sh

--- simple.sh
+++ simple.sh
@@ -1,5 +1,6 @@
-#!/usr/bin/env sh
+#!/usr/bin/env zsh
 apt-get install -y zsh git-core vim
 curl -L http://install.ohmyz.sh | sh
 sed -ie 's/robbyrussell/avit/g' ~/.zshrc
 curl https://raw.githubusercontent.com/khilnani/dot_files/master/vi/vimrc_safe -o ~/.vimrc
+curl -o- https://raw.githubusercontent.com/creationix/nvm/v0.33.1/install.sh | bash
