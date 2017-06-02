56a576bff4863794d1af86b4c84368a363b74738
khilnani <nik@khilnani.org>
2017-06-01 22:12:13 -0400

    updated

--- sh/ec2.sh
+++ sh/ec2.sh
@@ -1,8 +1,12 @@
 #!/bin/sh
 
-echo "Locating  Name: $2 ..."
-ID=`aws ec2 describe-instances --filters Name=tag:Name,Values=$2 --output text | gre
-p INSTANCE | awk '{ print $8; }'`
+if [ -n "$2" ]; then
+    echo "Locating: $1 ..."
+    ID=`aws ec2 describe-instances --filters Name=tag:Name,Values=$1 --output text | grep INSTANCE | awk '{ print $8; }'`
 
-echo "$1 ID: $ID ..."
-aws ec2 $1-instances --instance-ids $ID --output text
+    echo "ID: $ID Action: $2..."
+    aws ec2 $2-instances --instance-ids $ID --output text | grep STATE
+else
+    echo "Checking: $1 ..."
+    aws ec2 describe-instances --filters Name=tag:Name,Values=$1 --output text | grep STATE
+fi
