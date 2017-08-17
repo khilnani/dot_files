37402153a3ee95a5623f8fd65340f345a1169c5d
khilnani <nik@khilnani.org>
2017-08-17 02:11:27 +0000

    updated

--- sh/yj.sh
+++ sh/yj.sh
@@ -2,7 +2,7 @@
 
 if test -z "$2"
 then
-  echo "USAGE: $0 FILE.yml FILE.json"
+  echo "USAGE: $0 file.yml file.json"
 else
-  python -c 'import sys, yaml, json; json.dump(yaml.load(sys.stdin), sys.stdout, indent=4)' < $1.yaml > $2.json
+  python -c 'import sys, yaml, json; json.dump(yaml.load(sys.stdin), sys.stdout, indent=4)' < $1 > $2
 fi
