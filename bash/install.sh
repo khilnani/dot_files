f3b531e13e3bbec107024dc41f11f33e95262ede
khilnani <nik@khilnani.org>
2018-11-24 11:49:43 -0500

    o

--- bash/install.sh
+++ bash/install.sh
@@ -13,6 +13,7 @@ if [ "$(uname)" == "Darwin" ]; then
   echo "MacOS"
 elif [ "$(expr substr $(uname -s) 1 5)" == "Linux" ]; then
   echo "Ubuntu"
+  BINDIR=$HOME/bin
 elif [ "$(expr substr $(uname -s) 1 5)" == "MINGW" ]; then
   echo "Windows"
   BINDIR=$HOME/bin
