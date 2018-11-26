bf534e68ddb96699ed8edd28050da5c99cbdc4c7
khilnani <nik@khilnani.org>
2018-11-25 22:28:31 -0500

    added

--- bin/git-merge-to-master-squash.sh
+++ bin/git-merge-to-master-squash.sh
@@ -4,7 +4,7 @@ if [ "$#" -ne 1 ]; then
     echo "Please specify a branch name"
 else
 	git checkout master
-	git merge "$1"
+	git merge --squash "$1"
 fi
 
 
---
+++ bin/git-reset.sh
@@ -0,0 +1,5 @@
+#!/bin/sh
+
+git fetch origin
+git reset --hard origin
+
