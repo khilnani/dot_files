6a97bb5900a7cbed7df777014a4ae4f7ec690e49
khilnani <nik@khilnani.org>
2018-12-20 21:12:18 -0500

    fixed

--- bin/git-prune-file.sh
+++ bin/git-prune-file.sh
@@ -3,6 +3,6 @@
 if [ "$#" -ne 1 ]; then
   echo "Please provide a filename"
 else
-  git filter-branch --force --index-filter 'git rm --cached --ignore-unmatch $1' --prune-empty --tag-name-filter cat -- --all
+  git filter-branch --force --index-filter "git rm -r --cached --ignore-unmatch $1" --prune-empty --tag-name-filter cat -- --all
   git push origin --force --all
 fi
