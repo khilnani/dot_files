7a833b938e832bb77409c03251aada0d05b3c22b
khilnani <nik@khilnani.org>
2019-05-26 08:01:06 -0400

    updated

--- bin/ec2.sh
+++ bin/ec2.sh
@@ -1,12 +1,20 @@
 #!/bin/sh
 
+# USAGE: 
+# - STATUS: ec2.sh NAME
+# - START:  ec2.sh NAME start
+# - STOP:   ec2.sh NAME stop
+#
+# Prepend with AWS_PROFILE=NAME ec.sh ..  to specify an AWSprofile
+
+
 if [ -n "$2" ]; then
     echo "Locating: $1 ..."
     ID=`aws ec2 describe-instances --filters Name=tag:Name,Values=$1 --output text | grep INSTANCE | awk '{ print $8; }'`
 
     echo "ID: $ID Action: $2..."
-    aws ec2 $2-instances --instance-ids $ID --output text | grep STATE
+    aws ec2 $2-instances --instance-ids $ID --output text | grep 'CURRENTSTATE	'
 else
     echo "Checking: $1 ..."
-    aws ec2 describe-instances --filters Name=tag:Name,Values=$1 --output text | grep STATE
+    aws ec2 describe-instances --filters Name=tag:Name,Values=$1 --output text | grep 'STATE	'
 fi
