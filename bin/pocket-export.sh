f6530d7e8bcf6ab4e3b1561bdc7830b0cee60f0e
khilnani <nik@khilnani.org>
2019-05-27 11:14:07 -0400

    added

---
+++ bin/pocket-export.sh
@@ -0,0 +1,14 @@
+#!/usr/bin/env bash
+
+# see https://getpocket.com/developer/docs/v3/retrieve
+
+consumerkey=$1
+accesstoken=$2
+
+curl \
+--header "Content-type: application/json" \
+--request POST \
+--data "{\"state\": \"all\", \"sort\": \"newest\", \"detailType\": \"complete\", \"consumer_key\": \"$consumerkey\", \"access_token\": \"$accesstoken\"}" \
+https://getpocket.com/v3/get > export
+
+jq ".list[] | {sort_id: .sort_id, title: .resolved_title, url: .resolved_url, excerpt: .excerpt}" export > pocket-export.json
