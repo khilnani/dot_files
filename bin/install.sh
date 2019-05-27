d000c205a855c7076717d7c69a90f797e99e4703
khilnani <nik@khilnani.org>
2019-05-27 16:01:02 -0400

    updated

--- bin/install.sh
+++ bin/install.sh
@@ -5,24 +5,26 @@
 # Bin
 #############################################
 
-
+os=undefined
 BINDIR=/usr/local/bin
 
 
 if [ "$(uname)" == "Darwin" ]; then
-  echo "MacOS"
+  os="MacOS"
 elif [[ "$(uname -o)" == "Android" ]]; then
-  echo "Android"
+  os="Android"
   BINDIR=$HOME/bin
 elif [ "$(expr substr $(uname -s) 1 5)" == "Linux" ]; then
-  echo "Linux"
+  os="Linux"
   BINDIR=$HOME/bin
 elif [ "$(expr substr $(uname -s) 1 5)" == "MINGW" ]; then
-  echo "Windows"
+  os="Windows"
   BINDIR=$HOME/bin
 fi
 
-echo "---- Installing to $BINDIR"
+echo ""
+echo "OS: $os"
+echo "Installing to $BINDIR"
 
 
 if [ ! -d "$BINDIR" ]; then
