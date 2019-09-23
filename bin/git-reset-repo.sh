cc5972d8360940ac51893e023168ebf8ef4e54a6
khilnani <nik@khilnani.org>
2019-09-23 01:51:15 +0000

    updated

--- bin/git-reset-repo.sh
+++ bin/git-reset-repo.sh
@@ -1,13 +1,3 @@
 #!/bin/sh
 
-if [ "$#" -ne 1 ]; then
-  echo "Please provide a remote"
-else
-  cat .git/config
-  rm -rf .git
-  git init
-  git add .
-  git commit -am "Added"
-  git remote add origin "$1"
-  git push -u --force origin master
-fi
+git reset $(git commit-tree HEAD^{tree} -m "Commit history reset")
