17dd59ad0d45ff7c0af30fd166d11144f1925754
khilnani <nik@khilnani.org>
2019-12-21 20:09:16 +0000

    updated

--- home/install.sh
+++ home/install.sh
@@ -16,5 +16,7 @@ PS1='[\u@\h \W]\$ '
 alias ls='ls --color'
 LS_COLORS='ow=36;01:di=1:fi=0:ln=31:pi=5:so=5:bd=5:cd=5:or=31:mi=0:ex=36:*.rpm=90'
 export LS_COLORS
+
+export TERM=xterm-256color
 EOL
 source ~/.bashrc
