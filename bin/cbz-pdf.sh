c3bb4e2e1ebff72ffbb8751f1538d75d48139688
khilnani <nik@khilnani.org>
2023-04-08 11:25:34 -0400

    added

---
+++ bin/cbz-pdf.sh
@@ -0,0 +1,19 @@
+#!/bin/sh -x
+
+
+# Delete and recreate output directory
+# Loop through each file
+
+# brew install imagemagick
+
+for f in *.cbz
+do
+  [ -e "${f%.*}" ] && rm -rf "${f%.*}"
+  mkdir "${f%.*}"
+  unzip "$f" -d "${f%.*}/"
+  zip -r "${f%.*}.cbz" "${f%.*}"
+  convert "${f%.*}/*" "${f%.*}.pdf"
+  rm -rf "${f%.*}"
+done
+
+
